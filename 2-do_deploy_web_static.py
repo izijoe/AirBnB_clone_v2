197474211dd3c0ca2c4058e54596e85a69e11b06
izijeo <jospaco2001@yahoo.com>
2023-05-07 18:10:52 +0000

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 """
-script that distributes archive to webservers
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
-import os.path import exists
-from fabric.api import env, run, put, sudo
 
-env.hosts = ['54.237.112.44', '35.175.63.68']
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_deploy(archive_path):
-    """Copies archive file from local to my webservers"""
-
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        file_name = archive_path.split('/')[-1].split('.')[0]
-        file_folder = '/data/web_static/releases/{file_name}'
-        run(f"mkdir -p {file_folder}")
-        run(f"tar -xzf /tmp/{file_name}.tgz -C {file_folder}")
-        run(f'rm /tmp/{file_name}.tgz')
-        run(f'mv {file_folder}/web_static/* {file_folder}')
-        run('rm -rf {file_folder}/web_static')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run(f'ln -s {file_folder}/ /data/web_static/current')
-        print('New version deployed!')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception:
+    except:
         return False
